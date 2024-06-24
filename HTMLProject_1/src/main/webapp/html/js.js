cd34734ad85b3a1cd19e910c63dcebde909fa199
user <user@DESKTOP-VNHSN2U>
2024-06-24 17:51:00 +0900

    0624

---
+++ HTMLProject_1/src/main/webapp/html/js.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+function start1(){
+		alert("자바스크립트 시작1");
+	}
+	let start2=function(){
+		alert("자바스크립트 시작2")
+	}
+	// 자바스크립트 6버전에서 주로 사용 => Arrow 함수 => funtion , return을 제거해서 사용
+	// 자바 => 람다식
+	let start3=()=> {
+		alert("자바스크립스 시작3")
+	}
